9e2916256c3f52a42ebb4b00e6eba9e26143d383
Andrew <neborsky.andrew@gmail.com>
2018-10-29 18:49:02 +0300

    Исправление ошибок

--- HW_1/2.py
+++ HW_1/2.py
@@ -1,14 +1,11 @@
-L = input('Введите количество чисел: ')
-i = 0
+L = int(input('Введите количество чисел: '))
 s = 0
 
-while i < int(L):  # Лучше использовать for i in range(L)
+for i in range(L):
     a = input('Число №%d: ' % (i+1))
     a = int(a)
 
     if a % 3 == 0:
         s += a
-
-    i += 1
 
 print('Сумма чисел делящихся на 3 равна: %d' % s)
--- HW_1/4.py
+++ HW_1/4.py
@@ -1,12 +1,9 @@
 x = input('Введите строку: ')
 z = len(x)
-i = 0
 p = True
 
-while i <= z/2:  # как и во 2 второй задаче, лучше использовать for i in range(z // 2)
-    if x[i] == x[z - i - 1]:  # здесь можно использовать запись проще: x[i] == x[-1 - i]
-        i += 1
-    else:
+for i in range(z // 2):
+    if x[i] != x[- i - 1]:
         p = False
         break
 
