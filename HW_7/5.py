bec9c8e3d22ac308f26de768bd49a7403c994e28
Andrey Strelkov <a.p.strelkov@gmail.com>
2018-12-12 09:25:46 +0300

    Комментарии к задачам

--- HW_7/5.py
+++ HW_7/5.py
@@ -1,5 +1,5 @@
 from re import sub
 
 x = 'The dog trotted forward with a short, sharp bark, and the keeper lifted his face suddenly and saw her.'
-x = sub('[T|t]he |an? ', '', x)
+x = sub('[T|t]he |an? ', '', x)  # r'(^|\s)([Tt]he|[Aa]n?)(\s|$)'
 print(x)
