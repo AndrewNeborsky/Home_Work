e3ce2b348f2dd4d4cc55f150821f04c5c7a16ff0
Андрей <neborsky.andrew@gmail.com>
2019-01-13 21:43:56 +0300

    Исправление ошибок

--- HW_7/5.py
+++ HW_7/5.py
@@ -1,5 +1,5 @@
 from re import sub
 
 x = 'The dog trotted forward with a short, sharp bark, and the keeper lifted his face suddenly and saw her.'
-x = sub('[T|t]he |an? ', '', x)  # r'(^|\s)([Tt]he|[Aa]n?)(\s|$)'
+x = sub(r'(^|\s)([Tt]he|[Aa]n?)(\s|$)', ' ', x)
 print(x)
